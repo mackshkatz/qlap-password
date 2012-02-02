f14267e539b26c3b6e21360625f9fd65037168e4
Max Katz <maxwelljkatz@gmail.com>
2012-02-01 21:11:38 -0700

    An organization object is successfully created when I create a new User object, however I can't get the name to inherit properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
 	has_many :memberships
 	has_many :organizations, :through => :memberships
+
+	# When a user creates an account
+	after_create :create_organization
+
+	def create_organization
+		# A New organization should be created with the
+		# same name and be flagged as a single-person
+		# organization
+		Organization.create( :is_single_person => true, :name => User.name )
+	end
 end
